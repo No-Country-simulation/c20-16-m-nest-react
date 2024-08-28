c88eae2dc93bf19302d9740d36d78d6af61ffdd7
martinbejarano <martinbejarano12@outlook.com>
2024-08-28 16:56:35 -0300

    arreglo bug register page

--- front/src/app/(auth)/register/page.tsx
+++ front/src/app/(auth)/register/page.tsx
@@ -1,7 +1,78 @@
-export default function Register() {
+"use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/react";
+
+const formSchema = z
+  .object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(3),
+    passwordConfirm: z.string(),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "Las contraseñas no son iguales",
+    path: ["passwordConfirm"],
+  });
+
+export default function RegisterPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+  });
+
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div className="w-full min-h-screen">
-      <h1 className=" text-center py-10">Formulario registro</h1>
-    </div>
+    <main className="flex justify-center items-center min-h-screen p-12 bg-white">
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="flex flex-col gap-4 max-w-md w-full"
+      >
+        <Input
+          {...form.register("name")}
+          type="text"
+          label="Nombre completo"
+          variant="flat"
+          className="max-w-md"
+        />
+        <Input
+          {...form.register("email")}
+          label="Email"
+          variant="flat"
+          className="max-w-md"
+          isInvalid={!!form.formState.errors.email}
+          errorMessage={form.formState.errors.email?.message}
+        />
+        <Input
+          {...form.register("password")}
+          type="password"
+          label="Contraseña"
+          variant="flat"
+          className="max-w-md"
+        />
+        <Input
+          {...form.register("passwordConfirm")}
+          type="password"
+          label="Confirmar contraseña"
+          variant="flat"
+          className="max-w-md"
+          isInvalid={!!form.formState.errors.passwordConfirm}
+          errorMessage={form.formState.errors.passwordConfirm?.message}
+        />
+        <Button type="submit" className="w-full mt-6" color="primary">
+          Crear cuenta
+        </Button>
+      </form>
+    </main>
   );
 }
