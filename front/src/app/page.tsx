69082dc1e539c1a3acc205732276b977d13fef01
EzeNotebook <ezgarcia@frba.utn.edu.ar>
2024-09-02 15:35:52 -0300

    termiando el carrusel

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+import Carousel from "@/components/UI/Carousel/Carousel";
 import Header from "@/components/UI/Header";
+import { useTestCont } from "@/context/zustang";
 
 export default function Home() {
+  const { inc, count, dec }: any = useTestCont();
   return (
     <main>
       <Header />
+      {/*secciont carrusel-hero*/}
+      <section>
+        <Carousel />
+      </section>
+      <div>{count}</div>
+      <button className="border-2 border-black" onClick={inc}>
+        incrementar +1
+      </button>
+      <button className="border-2 border-black" onClick={dec}>
+        decrementa -1
+      </button>
     </main>
   );
 }
