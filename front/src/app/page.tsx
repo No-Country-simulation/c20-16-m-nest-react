90fb2594379d2d70327117855d4dac92c77b9da3
SamuDev <samudev01@gmail.com>
2024-09-03 13:41:46 -0300

    cambios menores

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/hero/Hero";
 import Services from "@/components/services/Services";
 import ContactUs from "@/components/UI/ContactUs";
+import Footer from "@/components/UI/Footer";
 import Header from "@/components/UI/Header";
 import { useTestCont } from "@/context/zustang";
 import { FaHouseChimney } from "react-icons/fa6";
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <Services />
       <ContactUs />
+      <Footer />
     </main>
   );
 }
