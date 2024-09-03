c9355021eca570ecc480c85f5703c4a33c5b176c
martinbejarano <martinbejarano12@outlook.com>
2024-09-03 01:26:30 -0300

    Formulario de contacto

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,24 +1,17 @@
-"use client";
 import Hero from "@/components/hero/Hero";
 import Services from "@/components/services/Services";
+import ContactUs from "@/components/UI/ContactUs";
 import Header from "@/components/UI/Header";
 import { useTestCont } from "@/context/zustang";
 import { FaHouseChimney } from "react-icons/fa6";
 
 export default function Home() {
-  const { inc, count, dec }: any = useTestCont();
   return (
     <main className="w-full">
       <Header />
       <Hero />
       <Services />
-      <div>{count}</div>
-      <button className="border-2 border-black" onClick={inc}>
-        incrementar +1
-      </button>
-      <button className="border-2 border-black" onClick={dec}>
-        decrementa -1
-      </button>
+      <ContactUs />
     </main>
   );
 }
