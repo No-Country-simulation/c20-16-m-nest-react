b24ec42ca1316593b925e12f28cc0cd75e5d7637
SamuDev <samudev01@gmail.com>
2024-09-06 12:06:42 -0300

    Actualización de Report Form Page

--- front/src/app/(auth)/reportForm/page.tsx
+++ front/src/app/(auth)/reportForm/page.tsx
@@ -1,15 +1,38 @@
+"use client"
+import React, { useState } from 'react';
 import FormReport from '@/components/reportForm/FormReport';
 import ImgLoader from '@/components/reportForm/ImgLoader';
 import Footer from '@/components/UI/Footer';
 import Header from '@/components/UI/Header';
 
-const ReportFormPage = () => {
+const ReportFormPage: React.FC = () => {
+    const [images, setImages] = useState<File[]>([]);
+
+    const handleImageUpload = (uploadedImages: File[]) => {
+        setImages(uploadedImages);
+    };
+
+    const handleSubmit = (formData: FormData) => {
+        console.log('Enviando datos del formulario...', formData);
+
+        // Para conexion back
+
+    };
+
     return (
-        <div>
+        <div className="min-h-screen flex flex-col">
             <Header />
-            <h1>Reportar Mascota</h1>
-            <FormReport />
-            <ImgLoader />
+            <div className="mx-auto px-4 pt-32 flex-grow flex justify-center items-center">
+                <div className="flex flex-col md:flex-row justify-center items-start space-y-6 md:space-y-0 md:space-x-3">
+                    <div className="w-full md:w-2/6">
+                        <h1 className="text-4xl text-primary font-bold mb-8">Reportar Mascota</h1>
+                        <FormReport images={images} onSubmit={handleSubmit} />
+                    </div>
+                    <div className="w-full min-h-screen py-24 md:w-1/2">
+                        <ImgLoader onImageUpload={handleImageUpload} />
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     );
