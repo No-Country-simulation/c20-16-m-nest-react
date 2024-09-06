1efd88f2fbdb430d95d2a19f9c9dd208b8a16ab5
EzeNotebook <ezgarcia@frba.utn.edu.ar>
2024-09-06 19:50:58 -0300

    responsivo y UI

--- front/src/app/(auth)/login/page.tsx
+++ front/src/app/(auth)/login/page.tsx
@@ -2,9 +2,23 @@ import FromLogin from "@/components/login/FormLogin";
 
 export default function login() {
   return (
-    <div className="w-full min-h-screen">
-      <h1 className=" text-center py-10">Formulario login</h1>
-      <FromLogin />
-    </div>
+    <main className="w-full min-h-screen flex justify-center md:justify-start items-center max-w-[1440px] mx-auto">
+      <section className="w-full max-w-md h-fit flex flex-col items-start gap-y-8 bg-white rounded-2xl p-4 md:bg-transparent">
+        <h1 className="w-full md:text-left text-center text-2xl sx:text-5xl text-primary font-semibold">
+          Iniciar Sesion
+        </h1>
+        <FromLogin />
+      </section>
+      <img
+        src="/svg/background-login-animals.svg"
+        className=" max-w-[474px] max-h-[345px] sm:max-w-[574px] sm:max-h-[445px] absolute bottom-0 right-0 sm:right-28 -z-40"
+        alt="Mascotas login"
+      />
+      <img
+        src="/svg/background-login-register.svg"
+        className="absolute top-0 right-0 w-full max-w-fit h-full object-cover -z-50 "
+        alt="fondo-login"
+      />
+    </main>
   );
 }
--- front/src/app/(auth)/report/page.tsx
+++ front/src/app/(auth)/report/page.tsx
@@ -1,115 +1,126 @@
-import React from 'react';
+import React from "react";
 import { ReportCard } from "@/components/report/ReportCard";
 import Footer from "@/components/UI/Footer";
 import Header from "@/components/UI/Header";
-import { FaSearch } from 'react-icons/fa';
-import ReportButton from '@/components/report/ReportButton';
+import { FaSearch } from "react-icons/fa";
+import ReportButton from "@/components/report/ReportButton";
 
 const ReportPage: React.FC = () => {
-    const reports = [
-        // EJEMPLO
-        {
-            title: "Perro perdido en Junín",
-            description: "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
-            location: "Junín",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-        {
-            title: "Perro perdido en La Plata",
-            description: "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
-            location: "La Plata",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-        {
-            title: "Perro perdido en Junín",
-            description: "En la noche este perro entró asustado y sucio a mi garaje, es pequeño. No sé si tendrá dueño.",
-            location: "Junín",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-        {
-            title: "Perro perdido en Junín",
-            description: "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
-            location: "Junín",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-        {
-            title: "Perro perdido en La Plata",
-            description: "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
-            location: "La Plata",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-        {
-            title: "Perro perdido en Junín",
-            description: "En la noche este perro entró asustado y sucio a mi garaje, es pequeño. No sé si tendrá dueño.",
-            location: "Junín",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-        {
-            title: "Perro perdido en Junín",
-            description: "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
-            location: "Junín",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-        {
-            title: "Perro perdido en La Plata",
-            description: "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
-            location: "La Plata",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-        {
-            title: "Perro perdido en Junín",
-            description: "En la noche este perro entró asustado y sucio a mi garaje, es pequeño. No sé si tendrá dueño.",
-            location: "Junín",
-            animalType: "Perro",
-            imageUrl: "./images/image-collage-5.png"
-        },
-    ];
+  const reports = [
+    // EJEMPLO
+    {
+      title: "Perro perdido en Junín",
+      description:
+        "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
+      location: "Junín",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+    {
+      title: "Perro perdido en La Plata",
+      description:
+        "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
+      location: "La Plata",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+    {
+      title: "Perro perdido en Junín",
+      description:
+        "En la noche este perro entró asustado y sucio a mi garaje, es pequeño. No sé si tendrá dueño.",
+      location: "Junín",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+    {
+      title: "Perro perdido en Junín",
+      description:
+        "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
+      location: "Junín",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+    {
+      title: "Perro perdido en La Plata",
+      description:
+        "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
+      location: "La Plata",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+    {
+      title: "Perro perdido en Junín",
+      description:
+        "En la noche este perro entró asustado y sucio a mi garaje, es pequeño. No sé si tendrá dueño.",
+      location: "Junín",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+    {
+      title: "Perro perdido en Junín",
+      description:
+        "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
+      location: "Junín",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+    {
+      title: "Perro perdido en La Plata",
+      description:
+        "Este perro estuvo afuera de mi casa llorando toda la noche, parece que sí tiene dueño porque se lo ve cuidado. No tiene collar.",
+      location: "La Plata",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+    {
+      title: "Perro perdido en Junín",
+      description:
+        "En la noche este perro entró asustado y sucio a mi garaje, es pequeño. No sé si tendrá dueño.",
+      location: "Junín",
+      animalType: "Perro",
+      imageUrl: "./images/image-collage-5.png",
+    },
+  ];
 
-    return (
-        <div className="flex flex-col min-h-screen text-white">
-            <Header />
-            <div className="pt-[2rem] flex-grow bg-[url('/images/background-report.png')] bg-bottom bg-no-repeat bg-cover overflow-hidden">
-                <div className="container mx-auto px-4 my-32 md:px-6 lg:px-8">
-                    <h1 className="text-4xl text-primary font-bold my-8 w-full md:w-3/4 lg:w-2/6">Reporte de mascotas</h1>
-                    <div className="flex flex-col items-start gap-4 mb-8">
-                        <div className="relative w-full md:w-3/4 lg:w-2/6">
-                            <input
-                                type="text"
-                                placeholder="Buscar mascota..."
-                                className="p-2 pr-10 rounded-full text-black w-full"
-                            />
-                            <FaSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-secondary-black text-2xl" />
-                        </div>
-                        <button className="p-2 bg-primary rounded-lg hover:bg-secondary-hovcolor transition-all duration-300">
-                            Filtrar
-                        </button>
-                    </div>
-                </div>
+  return (
+    <div className="flex flex-col min-h-screen text-white">
+      
+      <div className="pt-[2rem] flex-grow bg-[url('/images/background-report.png')] bg-bottom bg-no-repeat bg-cover overflow-hidden">
+        <div className="container mx-auto px-4 my-32 md:px-6 lg:px-8">
+          <h1 className="text-4xl text-primary font-bold my-8 w-full md:w-3/4 lg:w-2/6">
+            Reporte de mascotas
+          </h1>
+          <div className="flex flex-col items-start gap-4 mb-8">
+            <div className="relative w-full md:w-3/4 lg:w-2/6">
+              <input
+                type="text"
+                placeholder="Buscar mascota..."
+                className="p-2 pr-10 rounded-full text-black w-full"
+              />
+              <FaSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-secondary-black text-2xl" />
             </div>
-            <div className="container mx-auto px-4 py-8 md:px-6 lg:px-8 grid items-center justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-8">
-                {reports.map((report, index) => (
-                    <ReportCard
-                        key={index}
-                        title={report.title}
-                        description={report.description}
-                        location={report.location}
-                        animalType={report.animalType}
-                        imageUrl={report.imageUrl}
-                    />
-                ))}
-            </div>
-            <ReportButton />
-            <Footer />
+            <button className="p-2 bg-primary rounded-lg hover:bg-secondary-hovcolor transition-all duration-300">
+              Filtrar
+            </button>
+          </div>
         </div>
-    );
+      </div>
+      <div className="container mx-auto px-4 py-8 md:px-6 lg:px-8 grid items-center justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-8">
+        {reports.map((report, index) => (
+          <ReportCard
+            key={index}
+            title={report.title}
+            description={report.description}
+            location={report.location}
+            animalType={report.animalType}
+            imageUrl={report.imageUrl}
+          />
+        ))}
+      </div>
+      <ReportButton />
+     
+    </div>
+  );
 };
 
 export default ReportPage;
