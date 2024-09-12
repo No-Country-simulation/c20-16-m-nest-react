01831f1638301f1fa4b0cca41cfe6cd3a15285f9
SamuDev <samudev01@gmail.com>
2024-09-12 00:44:45 -0300

    Finalizando Reporte Formulario

--- front/src/components/reportForm/FormReport.tsx
+++ front/src/components/reportForm/FormReport.tsx
@@ -1,105 +1,133 @@
-"use client"
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Input, Textarea, Select, SelectItem } from '@nextui-org/react';
-import { Specie } from "@/interfaces/FormReport/specie"
-import { Sex } from "@/interfaces/FormReport/sex"
-import { Provincia } from "@/interfaces/FormReport/province"
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input, Textarea, Select, SelectItem } from "@nextui-org/react";
+import { Specie } from "@/interfaces/FormReport/specie";
+import { Sex } from "@/interfaces/FormReport/sex";
+import { Provincia } from "@/interfaces/FormReport/province";
 
 const formReportSchema = z.object({
     title: z.string().min(1, { message: "El título es obligatorio" }),
     description: z.string().min(1, { message: "La descripción es obligatoria" }),
-    species: z.enum(['dog', 'cat'], {
-        errorMap: () => ({ message: "La especie es obligatoria" })
+    species: z.enum(["dog", "cat"], {
+        errorMap: () => ({ message: "La especie es obligatoria" }),
     }),
-    sex: z.enum(['macho', 'hembra'], {
-        errorMap: () => ({ message: "El sexo es obligatorio" })
+    sex: z.enum(["macho", "hembra"], {
+        errorMap: () => ({ message: "El sexo es obligatorio" }),
     }),
-    province: z.enum([
-        'buenos_aires', 'catamarca', 'chaco', 'chubut', 'cordoba', 'corrientes',
-        'entre_rios', 'formosa', 'jujuy', 'la_pampa', 'la_rioja', 'mendoza',
-        'misiones', 'neuquen', 'rio_negro', 'salta', 'san_juan', 'san_luis',
-        'santa_cruz', 'santa_fe', 'santiago_del_estero', 'tierra_del_fuego', 'tucuman'
-    ], {
-        errorMap: () => ({ message: "La provincia es obligatoria" })
-    }),
+    province: z.enum(
+        [
+            "buenos_aires",
+            "catamarca",
+            "chaco",
+            "chubut",
+            "cordoba",
+            "corrientes",
+            "entre_rios",
+            "formosa",
+            "jujuy",
+            "la_pampa",
+            "la_rioja",
+            "mendoza",
+            "misiones",
+            "neuquen",
+            "rio_negro",
+            "salta",
+            "san_juan",
+            "san_luis",
+            "santa_cruz",
+            "santa_fe",
+            "santiago_del_estero",
+            "tierra_del_fuego",
+            "tucuman",
+        ],
+        {
+            errorMap: () => ({ message: "La provincia es obligatoria" }),
+        }
+    ),
     locality: z.string().min(1, { message: "La localidad es obligatoria" }),
     street: z.string().min(1, { message: "La calle es obligatoria" }),
-    number: z.number({ invalid_type_error: "Debe ingresar un número" })
+    number: z
+        .number({ invalid_type_error: "Debe ingresar un número" })
         .min(1, { message: "La altura es obligatoria" }),
-    postalCode: z.number({ invalid_type_error: "Debe ingresar un número" })
+    postalCode: z
+        .number({ invalid_type_error: "Debe ingresar un número" })
         .min(1, { message: "El código postal es obligatorio" }),
 });
 
 const species: Specie[] = [
-    { key: 'dog', label: 'Perro' },
-    { key: 'cat', label: 'Gato' },
+    { key: "dog", label: "Perro" },
+    { key: "cat", label: "Gato" },
 ];
 
 const sexs: Sex[] = [
-    { key: 'macho', label: 'Macho' },
-    { key: 'hembra', label: 'Hembra' },
+    { key: "macho", label: "Macho" },
+    { key: "hembra", label: "Hembra" },
 ];
 
 const provincias: Provincia[] = [
-    { key: 'buenos_aires', label: 'Buenos Aires' },
-    { key: 'catamarca', label: 'Catamarca' },
-    { key: 'chaco', label: 'Chaco' },
-    { key: 'chubut', label: 'Chubut' },
-    { key: 'cordoba', label: 'Córdoba' },
-    { key: 'corrientes', label: 'Corrientes' },
-    { key: 'entre_rios', label: 'Entre Ríos' },
-    { key: 'formosa', label: 'Formosa' },
-    { key: 'jujuy', label: 'Jujuy' },
-    { key: 'la_pampa', label: 'La Pampa' },
-    { key: 'la_rioja', label: 'La Rioja' },
-    { key: 'mendoza', label: 'Mendoza' },
-    { key: 'misiones', label: 'Misiones' },
-    { key: 'neuquen', label: 'Neuquén' },
-    { key: 'rio_negro', label: 'Río Negro' },
-    { key: 'salta', label: 'Salta' },
-    { key: 'san_juan', label: 'San Juan' },
-    { key: 'san_luis', label: 'San Luis' },
-    { key: 'santa_cruz', label: 'Santa Cruz' },
-    { key: 'santa_fe', label: 'Santa Fe' },
-    { key: 'santiago_del_estero', label: 'Santiago del Estero' },
-    { key: 'tierra_del_fuego', label: 'Tierra del Fuego' },
-    { key: 'tucuman', label: 'Tucumán' },
-]
-
+    { key: "buenos_aires", label: "Buenos Aires" },
+    { key: "catamarca", label: "Catamarca" },
+    { key: "chaco", label: "Chaco" },
+    { key: "chubut", label: "Chubut" },
+    { key: "cordoba", label: "Córdoba" },
+    { key: "corrientes", label: "Corrientes" },
+    { key: "entre_rios", label: "Entre Ríos" },
+    { key: "formosa", label: "Formosa" },
+    { key: "jujuy", label: "Jujuy" },
+    { key: "la_pampa", label: "La Pampa" },
+    { key: "la_rioja", label: "La Rioja" },
+    { key: "mendoza", label: "Mendoza" },
+    { key: "misiones", label: "Misiones" },
+    { key: "neuquen", label: "Neuquén" },
+    { key: "rio_negro", label: "Río Negro" },
+    { key: "salta", label: "Salta" },
+    { key: "san_juan", label: "San Juan" },
+    { key: "san_luis", label: "San Luis" },
+    { key: "santa_cruz", label: "Santa Cruz" },
+    { key: "santa_fe", label: "Santa Fe" },
+    { key: "santiago_del_estero", label: "Santiago del Estero" },
+    { key: "tierra_del_fuego", label: "Tierra del Fuego" },
+    { key: "tucuman", label: "Tucumán" },
+];
 
 type FormData = z.infer<typeof formReportSchema>;
 
 interface FormReportProps {
     onSubmit: (data: FormData) => void;
+    images: File[];
 }
 
-const FormReport: React.FC<FormReportProps> = ({ onSubmit }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+const FormReport: React.FC<FormReportProps> = ({ onSubmit, images }) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>({
         resolver: zodResolver(formReportSchema),
     });
 
     return (
-        <form className="space-y-4 py-8" onSubmit={handleSubmit(onSubmit)}>
+        <form className="space-y-4 m-4" onSubmit={handleSubmit(onSubmit)}>
             <Input
-                {...register('title')}
+                {...register("title")}
                 label="Título"
-                variant='flat'
+                variant="flat"
                 isInvalid={!!errors.title}
                 errorMessage={errors.title?.message}
             />
             <Textarea
-                {...register('description')}
+                {...register("description")}
                 label="Descripción"
-                variant='flat'
+                variant="flat"
                 rows={2}
                 isInvalid={!!errors.description}
                 errorMessage={errors.description?.message}
             />
             <Select
-                {...register('species')}
+                {...register("species")}
                 label="Selecciona especie"
                 isInvalid={!!errors.species}
                 errorMessage={errors.species?.message}
@@ -111,7 +139,7 @@ const FormReport: React.FC<FormReportProps> = ({ onSubmit }) => {
                 ))}
             </Select>
             <Select
-                {...register('sex')}
+                {...register("sex")}
                 label="Selecciona el sexo"
                 isInvalid={!!errors.sex}
                 errorMessage={errors.sex?.message}
@@ -123,7 +151,7 @@ const FormReport: React.FC<FormReportProps> = ({ onSubmit }) => {
                 ))}
             </Select>
             <Select
-                {...register('province')}
+                {...register("province")}
                 label="Selecciona provincia"
                 isInvalid={!!errors.province}
                 errorMessage={errors.province?.message}
@@ -135,49 +163,45 @@ const FormReport: React.FC<FormReportProps> = ({ onSubmit }) => {
                 ))}
             </Select>
             <Input
-                {...register('locality')}
+                {...register("locality")}
                 label="Localidad"
-                variant='flat'
+                variant="flat"
                 isInvalid={!!errors.locality}
                 errorMessage={errors.locality?.message}
             />
             <div className="w-full flex space-x-4">
                 <Input
-                    {...register('street')}
+                    {...register("street")}
                     label="Calle"
-                    variant='flat'
+                    variant="flat"
                     isInvalid={!!errors.street}
                     errorMessage={errors.street?.message}
-                    className='w-4/6'
+                    className="w-4/6"
                 />
                 <Input
-                    {...register('number', { valueAsNumber: true })}
+                    {...register("number", { valueAsNumber: true })}
                     label="Altura"
-                    variant='flat'
+                    variant="flat"
                     isInvalid={!!errors.number}
                     errorMessage={errors.number?.message}
-                    className='w-2/6 '
+                    className="w-2/6"
                 />
             </div>
             <Input
-                {...register('postalCode', { valueAsNumber: true })}
+                {...register("postalCode", { valueAsNumber: true })}
                 label="Código Postal"
-                variant='flat'
+                variant="flat"
                 isInvalid={!!errors.postalCode}
                 errorMessage={errors.postalCode?.message}
             />
             <button
                 type="submit"
-                className="w-3/6 lg:w-2/6 p-2 bg-primary text-white rounded-3xl hover:bg-accent hover:duration-300"
+                className="w-3/6 lg:w-2/6 p-2 m-4 bg-primary text-white rounded-3xl hover:bg-accent hover:duration-300"
             >
                 Enviar
             </button>
         </form>
-
     );
-
-    // falta definir/manejar el botón de envío, por eso el error en ReportFormPage
-
 };
 
 export default FormReport;
