46d8e152fd19beeaf7c6f2237789c7de318e2c4d
EzeNotebook <ezgarcia@frba.utn.edu.ar>
2024-09-17 17:10:29 -0300

    arreglando erroes en el formulario con los label y utilizando el useForm

--- front/src/components/Donation/ContactFormCarousel.tsx
+++ front/src/components/Donation/ContactFormCarousel.tsx
@@ -1,6 +1,7 @@
 "use client";
 import CarouselDonation from "@/components/Donation/CarouselDonation";
 import { Input, Select, SelectItem } from "@nextui-org/react";
+import { useForm } from "react-hook-form";
 
 const listImages: { url: string; alt: string }[] = [
   { url: "/images/image-donation-1.png", alt: "imagen-refugio-1" },
@@ -8,15 +9,35 @@ const listImages: { url: string; alt: string }[] = [
   { url: "/images/image-donation-3.png", alt: "imagen-refugio-3" },
 ];
 export default function () {
+  const { register, handleSubmit, reset } = useForm();
   return (
-    <section className="px-3 flex flex-col md:flex-row justify-between items-center gap-20 mx-auto w-full max-w-[1440px] py-12" id="contact">
+    <section
+      className="px-3 flex flex-col md:flex-row justify-between items-center gap-20 mx-auto w-full max-w-[1440px] py-12"
+      id="contact"
+      onSubmit={handleSubmit((data) => {
+        reset(), console.log(data);
+      })}
+    >
       <article className="w-full md:w-1/2 flex flex-col gap-y-6">
         <h2 className="text-3xl md:text-4xl font-semibold text-primary text-center">
           Donacion
         </h2>
         <form className=" flex flex-col gap-y-5">
-          <Input variant="flat" placeholder="Nombre" />
-          <Select placeholder="Selecciona refugio" className="">
+          <Input
+            {...register("name")}
+            name="name"
+            variant="flat"
+            placeholder="Ej: Sebastian"
+            labelPlacement="outside"
+            label="Nombre"
+          />
+          <Select
+            {...register("shelter")}
+            placeholder="Selecciona un refugio"
+            name="shelter"
+            labelPlacement="outside"
+            label="Refugio"
+          >
             <SelectItem key={1}>refugio 1</SelectItem>
             <SelectItem key={2}>refugio 2</SelectItem>
             <SelectItem key={3}>refugio 3</SelectItem>
@@ -25,6 +46,8 @@ export default function () {
             type="number"
             placeholder="0.00"
             labelPlacement="outside"
+            label="Precio"
+            {...register("price")}
             startContent={
               <div className="pointer-events-none flex items-center">
                 <span className="text-default-400 text-small">$</span>
